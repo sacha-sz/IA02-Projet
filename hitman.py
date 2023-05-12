a1661a24bba7571a39ed251c977f207bd612934a
lucascolas <lucas.colas13@gmail.com>
2023-05-12 17:35:07 +0200

    variables

--- hitman.py
+++ hitman.py
@@ -1,5 +1,48 @@
+"""
+Map : List[List[str]]
+    "r" : rien
+    "c" : costume
+    "k" : corde
+    "t" : cible/target 
+    "m" : mur
+    "gn" : garde nord
+    "gs" : garde sud
+    "ge" : garde est
+    "go" : garde ouest
+    "cn" : civil nord
+    "cs" : civil sud
+    "ce" : civil est
+    "co" : civil ouest
+    "hn" : hitman nord
+    "hs" : hitman sud
+    "he" : hitman est
+    "ho" : hitman ouest
+
+
+"""
+rien = "r"
+costume = "c"
+corde = "k"
+cible = "t"
+mur = "m"
+garde_nord = "gn"
+garde_sud = "gs"
+garde_est = "ge"
+garde_ouest = "go"
+civil_nord = "cn"
+civil_sud = "cs"
+civil_est = "ce"
+civil_ouest = "co"
+hitman_nord = "hn"
+hitman_sud = "hs"
+hitman_est = "he"
+hitman_ouest = "ho"
+
+
 def main():
     print("HITMAN")
+
+
     
 if __name__ == "__main__":
     main()
